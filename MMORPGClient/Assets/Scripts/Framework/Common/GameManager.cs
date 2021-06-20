e2fa1739954928d30605f756914d93cceb13678f
QDYD-max <2399739502@qq.com>
2021-06-21 00:18:42 +0800

    feat:TcpClient

--- MMORPGClient/Assets/Scripts/Framework/Common/GameManager.cs
+++ MMORPGClient/Assets/Scripts/Framework/Common/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Network;
 using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
@@ -8,6 +9,7 @@ public class GameManager : MonoSingleton<GameManager>
     private void Awake()
     {
         XLuaManager.Instance.Init();
+        TcpNetwork test = new TcpNetwork();
     }
 
     // Start is called before the first frame update
--- MMORPGClient/Assets/Scripts/Framework/Socket/Network/TcpNetwork.cs
+++ MMORPGClient/Assets/Scripts/Framework/Socket/Network/TcpNetwork.cs
@@ -1,9 +1,33 @@
+using System.Net;
+using System.Net.Sockets;
+using UnityEditor.Media;
 using UnityEngine;
 
 namespace Network
 {
     public class TcpNetwork
     {
+        public TcpClient TcpClient = null;
+        public NetworkStream NetworkStream = null;
+
+        public TcpNetwork()
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes("aaa");
+            TcpClient = new TcpClient("192.168.43.130", 8888);
+            NetworkStream = TcpClient.GetStream();
+            NetworkStream.Write(data, 0, data.Length);
+        }
+
+        void Send(string message)
+        {
+            byte[] data = System.Text.Encoding.Default.GetBytes(message);
+        }
+        
+
+        byte[] Read()
+        {
+            return null;
+        }
         
     }
 }
